002483d3a489d9b6b7aea40c310c09120b40d8c7
KinderPinGuiin <jordan.teltel.2001@gmail.com>
2021-06-14 22:56:23 +0200

    Problème interface résolu

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -82,7 +82,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      *
      * @see UserInterface
      */
-    public function getUserIdentifier(): string
+    public function getUsername(): string
     {
         return (string) $this->email;
     }

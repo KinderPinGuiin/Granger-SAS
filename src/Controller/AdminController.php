748f303cfafa4cde30b7ce9c9fc6da99f3620df7
KinderPinGuiin <jordan.teltel.2001@gmail.com>
2021-06-15 21:31:56 +0200

    Retrait des use inutiles

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,9 +2,7 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 /**

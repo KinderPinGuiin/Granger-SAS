0f48f1440e41235e3162a283f94e740e89d26ce5
KinderPinGuiin <jordan.teltel.2001@gmail.com>
2021-06-15 16:14:59 +0200

    Retrait de lignes de test dans le HomeController 2

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,8 +2,6 @@
 
 namespace App\Controller;
 
-use App\Utils\Constants;
-use App\Utils\GoogleDriveManager;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;

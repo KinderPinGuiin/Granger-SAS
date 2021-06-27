1671f2bada0b20836150457eca0ccdcc35dca20a
KinderPinGuiin <jordan.teltel.2001@gmail.com>
2021-06-27 20:58:40 +0200

    Ajout asserts Contenu

--- src/Entity/Contenu.php
+++ src/Entity/Contenu.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ContenuRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=ContenuRepository::class)
@@ -19,6 +20,7 @@ class Contenu
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(max=255)
      */
     private $page;
 
